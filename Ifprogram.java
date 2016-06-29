4eb1945bb8216a3e1a972828cfc476e9ee530bf8
ShriVanadhi <shrivanadhi@gmail.com>
2016-06-29 21:41:32 +0530

    Create Ifprogram.java

---
+++ Ifprogram.java
@@ -0,0 +1,27 @@
+package javaapplication1;
+
+public class Ifprogram {
+
+    public static void main(String[] args){
+            int i= 15;
+            // checks the boolean condtion and assigns to variable cond
+        boolean cond = 4!=4;
+        //print whether the boolean value is true or false
+        System.out.println(cond);
+       // checks if i value less than 10 
+    if(i<10){
+        //prints the command
+    System.out.println("Yes! It's true");
+            }
+    //else if checks if i value greater than 20
+    else if(i>20){
+        //prints the statement
+    System.out.println("No! It's false");
+       }
+    //else print the statement
+    else{
+        System.out.println("None is true");
+       }
+    }
+    
+}
