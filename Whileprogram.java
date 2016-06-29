e166ed22e152344d029ee0944defc9b910b5febf
ShriVanadhi <shrivanadhi@gmail.com>
2016-06-29 21:33:42 +0530

    Create Whileprogram.java

---
+++ Whileprogram.java
@@ -0,0 +1,23 @@
+package javaapplication1;
+
+public class Whileprogram {
+    public static void main(String[] args){
+        /*
+        *Intializing the variable, value = 0 
+        */
+        int value = 0;
+        /*
+        *while loop executes till the value less than 10
+        */
+        while(value<10){
+            /*
+        *this is a comment
+        */
+            System.out.println("test" + " " + value);
+            /*
+        *value increases by 1 at each execution
+        */
+        value = value+1;
+    }
+  }
+}
