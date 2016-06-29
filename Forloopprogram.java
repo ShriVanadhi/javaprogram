af8958a16afef16a26e0e83a1d0b646635138786
ShriVanadhi <shrivanadhi@gmail.com>
2016-06-29 21:37:46 +0530

    Create Forloopprogram.java

---
+++ Forloopprogram.java
@@ -0,0 +1,12 @@
+package javaapplication1;
+ 
+public class Forloopprogram {
+    public static void main(String[] args){
+            //intializing i is 0 and incrementing till 5 using for loop
+        for(int i=0;i<5;i++){
+        //Printing the value of i until the increment continues
+            System.out.println("value of i is" + " " + i);
+       }
+    }
+    
+}
