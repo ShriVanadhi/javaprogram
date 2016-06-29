19958e25601200cf2a99d6c1c6a54224bc74ea23
ShriVanadhi <shrivanadhi@gmail.com>
2016-06-29 21:30:29 +0530

    Create Hellopgm.java

---
+++ Hellopgm.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+public class Hellopgm {
+     /**
+         * main() function executes while java program runs     
+         * public = accessible to all
+         * void = returns nothing
+     * @param args
+         */
+    public static void main(String[] args){
+        /**
+     * this is a comment
+     */
+     System.out.println("Hello World");
+    }
+}
